fa33a3d5d780bc86d5cd79247df0b3e3ab6f800d
xuAMINE22 <uhammudz@gmail.com>
2023-12-27 20:13:00 -0800

    made the cart count interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 let productsHTML = '';
+let checkoutHTML = '';
 
 products.forEach(product => {
     productsHTML += `
@@ -46,7 +47,7 @@ products.forEach(product => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id = "${product.id}">
+            <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id  = "${product.id}">
             Add to Cart
             </button>
         </div>
@@ -58,6 +59,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart-button').forEach( button => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
+
         let matchingItem;
 
         cart.forEach( item => {
@@ -74,7 +76,18 @@ document.querySelectorAll('.js-add-to-cart-button').forEach( button => {
                 quantity: 1
             });
         };
+
+        let cartQuantity = 0;
+        
+        cart.forEach( item => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML
+        = cartQuantity;
+        
         
         console.log(cart);
+        console.log(cartQuantity);
     })
-})
+});
